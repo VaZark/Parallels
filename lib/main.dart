757c4c73ad231bfeb25eae17533df163095a0728
Vasanth M <vasanth93067@gmail.com>
2026-09-20 22:35:55 +0200

    switch to shadcn

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:shadcn_flutter/shadcn_flutter.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,8 +9,33 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return ShadcnApp(
+      scaling: AdaptiveScaling(1.15),
+      theme: ThemeData(
+        colorScheme: ColorSchemes.darkSlate.emerald,
+        radius: 0.25,
+        density: Density.reducedDensity,
+        surfaceOpacity: 0.9,
+        surfaceBlur: 4,
+      ),
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Basic(
+        title: Text('Hello World'),
+        leading: Icon(LucideIcons.star),
+        trailing: Icon(LucideIcons.arrowRight),
+        subtitle: Text('Subtitle'),
+        content: Text('Lorem ipsum dolor sit amet'),
+    )
+    ).center();
   }
 }
